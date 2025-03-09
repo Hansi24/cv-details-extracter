060c404c76a36d2037543e5ac3a2236992cf6b20
Hansi24 <126888276+Hansi24@users.noreply.github.com>
2025-03-09 14:42:06 +0530

    Update index.ts
    changed again to production mode

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -90,7 +90,7 @@ app.post("/api/update/application", async (req: any, res: any) => {
     const metaData = {
       "applicant_name": document.name,
       "email": document.email,
-      "status": "testing",
+      "status": "prod",
       "cv_processed": document.status === "PROCESSED",
       "processed_timestamp": document.createdAt,
     };
