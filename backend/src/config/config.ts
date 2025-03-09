adbf7f039159361fa89a2c1aa299fbbee776120a
Hansi <hansisewwandi0824@gmail.com>
2025-03-09 13:27:02 +0530

    cv-extractor

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -12,10 +12,10 @@ interface Config {
 }
 
 const config: Config = {
-    DB_URI: process.env.DB_URI || 'mongodb://127.0.0.1:27017/sums',
+    DB_URI: process.env.DB_URI || 'mongodb://127.0.0.1:27017/jobApplication',
     PORT: process.env.PORT ? parseInt(process.env.PORT) : 5001,
-    EMAIL: process.env.EMAIL || 'diveshdivesh1222@gmail.com',
-    APP_PASSWORD: process.env.APP_PASSWORD || 'divesh@123',
+    EMAIL: process.env.EMAIL || 'jfinder424@gmail.com',
+    APP_PASSWORD: process.env.APP_PASSWORD || 'okgxzqkasemfrhhx',
     S3BucketURl: process.env.S3BucketURl || 'https://cvdrivebucket.s3.us-east-1.amazonaws.com/',
     CandidateEmail: process.env.CandidateEmail || 'hansisewwandi0824@gmail.com',
 };
